868f122995c7f02dac22854a5915d52c3a2e749b
minashan <naro2310@mail.ru>
2022-08-29 18:57:01 +0400

    Add new activity_skripichnyy.xml and setOnClickListener for fab1

--- app/src/main/java/ru/mshamaev/doremishka/MainActivity.kt
+++ app/src/main/java/ru/mshamaev/doremishka/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.mshamaev.doremishka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab1 : View = findViewById(R.id.fab1)
+        fab1.setOnClickListener{
+            val intent: Intent = Intent(this, SkripichnyyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
